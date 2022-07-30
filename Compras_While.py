f696263663e353c2f4ccee87cc2811760026b38e
Yzero02 <92699414+Yzero02@users.noreply.github.com>
2022-07-30 19:57:04 -0300

    Enviando Projeto de compras

---
+++ Compras_While.py
@@ -0,0 +1,8 @@
+"""
+
+                                                                Comprando Frutas    by:Geronimo Neto
+
+"""
+produtos = {}
+while True:
+    produtos["Produto: "] = input("Qual Produto deseja comprar [ 1 ] Melancia  [ 2 ] Melão  [ 3 ] Abóbora: ")
