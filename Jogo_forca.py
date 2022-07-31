22b331c3cd0ed1c4191b79203d91fe0756fae5be
geronimo morais <geronimomorais1617@gmail.com>
2022-07-31 12:51:20 -0300

    Enviando Primeiro arquivo

---
+++ Jogo_forca.py
@@ -0,0 +1,14 @@
+"""
+
+                                                                    Jogo da forca    by: Geronimo Neto
+
+
+"""
+import random 
+palavra_chave = ["Sport","Vasco","Corinthias","Flamengo","Palmeiras"]
+palavra_selecionada = random.choice(palavra_chave)
+
+print(palavra_selecionada)
+
+print("--")
+print("Aula2")
