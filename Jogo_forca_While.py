f7df799e0b966a3ef376f6f2ee0c21e80040f0aa
geronimo morais <geronimomorais1617@gmail.com>
2022-08-03 21:01:24 -0300

    Enviando modificação na branch_nova

--- Jogo_forca_While.py
+++ Jogo_forca_While.py
@@ -16,14 +16,4 @@ print("Tente adivinhar qual palavra eu estou pensando ...".center(20))
 vazio = ""
 print()
 
-while chances <=3:
-    palavra_jogador = str(input("Digite uma letra: ")[0].upper())
-
-    for p in palavra_selecionada:
-
-        if p == palavra_jogador:
-            print(p,end="")
-            lista_de_palavras.append(p)
-        else:
-            print("*",end="")
 
